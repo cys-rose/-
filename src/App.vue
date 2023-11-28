bcf56ed0f3be956f54a9eab5b910c062257211c3
Rose <2677596161@qq.com>
2023-11-28 23:09:33 +0800

    图标引入

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
 
 </script>
 
 <template>
   <!-- 一级路由出口组件 -->
-  <RouterView></RouterView>
+  <RouterView>
+   
+  </RouterView>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.test{
+  color: $priceColor;
+}
 </style>
